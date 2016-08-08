be917fa79a337fdb641a7650d3d4f81700b00748
Chris Downie <cdownie@gmail.com>
2016-08-08 15:40:42 -0600

    Initial Commit

---
+++ cert-wallet/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  cert-wallet
+//
+//  Created by Chris Downie on 8/8/16.
+//  Copyright © 2016 Learning Machine. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
