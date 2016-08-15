03b69de2d5b631ca04900631784a62ca54de0de3
Chris Downie <cdownie@gmail.com>
2016-08-15 15:26:48 -0600

    Adds example certs and begins parsing them into Certificate objects
    * Tests are added for our v1.1 cert
    * Dummy objects are created for everything but Verify right now
    * The tests on the Verify object are passing, so I know this approach will work for the rest of them.

--- Models/Certificate.swift
+++ Models/Certificate.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+//import UIKit
 
 enum RevokeError : Error {
     case notImplemented
@@ -18,6 +19,7 @@ struct Certificate {
     let description: String
     let image : Data
     let language : String
+    let id : URL
     
     let issuer : Issuer
     let recipient : Recipient
@@ -25,8 +27,33 @@ struct Certificate {
     let verifyData : Verify
     
     // Not sure if this is better as a static func or an initialization function. This has the fewest 
-    static func from(file: Data) {
+    static func from(file: Data) -> Certificate? {
+        // Deserialize JSON
+        var json: [String: AnyObject]
+        do {
+            try json = JSONSerialization.jsonObject(with: file, options: []) as! [String: AnyObject]
+        } catch {
+            return nil
+        }
         
+        // Creating dummy objects with terrible values to pass the type checker for now
+        let issuer = Issuer(name: "", email: "", image: Data(), id: URL(string: "https://google.com")!, url: URL(string: "https://google.com")!, publicKey: "", publicKeyAddress: URL(string: "https://google.com")!, requestUrl: URL(string: "https://google.com")!)
+        let recipient = Recipient(givenName: "", familyName: "", identity: "", identityType: "", isHashed: false, publicKey: "")
+        let assertion = Assertion(issuedOn: Date(), signatureImage: Data(), evidence: "", uid: "", id: URL(string: "https://google.com")!)
+        
+        
+        // This is how I expect to be able to parse the JSON into the typed object we've defined.
+        guard let verifyData = json["verify"] as? [String : String],
+            let signer = verifyData["signer"],
+            let signedAttribute = verifyData["attribute-signed"],
+            let type = verifyData["type"],
+            let signerUrl = URL(string: signer) else {
+                return nil
+        }
+        let verify = Verify(signer: signerUrl, signedAttribute: signedAttribute, type: type)
+        
+        
+        return Certificate(title: "", subtitle: nil, description: "", image: Data(), language: "", id: URL(string: "https://google.com")!, issuer: issuer, recipient: recipient, assertion: assertion, verifyData: verify)
     }
     
     func toFile() -> Data {
---
+++ cert-walletTests/CertificateTests.swift
@@ -0,0 +1,101 @@
+//
+//  CertificateTests.swift
+//  cert-wallet
+//
+//  Created by Chris Downie on 8/15/16.
+//  Copyright © 2016 Digital Certificates Project. All rights reserved.
+//
+
+import XCTest
+
+class CertificateTests: XCTestCase {
+    let certificateFilename = "sample_unsigned_cert-1.1.0"
+    var file : Data? = nil
+    
+    override func setUp() {
+        super.setUp()
+
+        let testBundle = Bundle(for: self.dynamicType)
+        guard let fileUrl = testBundle.url(forResource: certificateFilename, withExtension: "json") else {
+            file = nil
+            return
+        }
+        
+        file = try? Data(contentsOf: fileUrl)
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        file = nil
+    }
+    
+    func testImportProperties() {
+        XCTAssertNotNil(file)
+        guard let file = file,
+            let cert = Certificate.from(file: file) else {
+                XCTFail("Failed to laod the test file in CertificateTests")
+                return
+        }
+        XCTAssertEqual(cert.title, "Certificate title")
+        XCTAssertEqual(cert.subtitle, "2016")
+        XCTAssertEqual(cert.description, "Certificate description")
+        XCTAssertEqual(cert.id, URL(string: "https://www.theissuer.edu/criteria/2016/05/certificate-type.json"))
+    }
+    
+    func testImportIssuerProperties() {
+        guard let file = file,
+            let cert = Certificate.from(file: file) else {
+                XCTFail("Failed to laod the test file in CertificateTests")
+                return
+        }
+        let issuer = cert.issuer
+        
+        XCTAssertEqual(issuer.name, "Issuing Institution")
+        XCTAssertEqual(issuer.email, "issuer@theissuer.edu")
+        XCTAssertEqual(issuer.id, URL(string: "https://www.theissuer.edu/issuer/the-issuer.json"))
+        XCTAssertEqual(issuer.url, URL(string: "http://www.theissuer.edu"))
+    }
+    
+    func testImportRecipientProperties() {
+        guard let file = file,
+            let cert = Certificate.from(file: file) else {
+                XCTFail("Failed to laod the test file in CertificateTests")
+                return
+        }
+        let recipient = cert.recipient
+        XCTAssertEqual(recipient.givenName, "RecipientFirstName")
+        XCTAssertEqual(recipient.familyName, "RecipientLastName")
+        XCTAssertEqual(recipient.identity, "recipient@domain.com")
+        XCTAssertEqual(recipient.identityType, "email")
+        XCTAssertEqual(recipient.isHashed, false)
+        XCTAssertEqual(recipient.publicKey, "n1EduLzKsTL1pM8Roz9vEV16AQnBdg9JCx")
+    }
+    
+    func testImportAssertionProperties() {
+        guard let file = file,
+            let cert = Certificate.from(file: file) else {
+                XCTFail("Failed to laod the test file in CertificateTests")
+                return
+        }
+        let assertion = cert.assertion
+        
+        XCTAssertEqual(assertion.issuedOn, Date(timeIntervalSinceReferenceDate: 485978400))
+        XCTAssertEqual(assertion.evidence, "")
+        XCTAssertEqual(assertion.uid, "68656c6c6f636f6d7077ffff")
+        XCTAssertEqual(assertion.id, URL(string: "http://www.theissuer.edu/68656c6c6f636f6d7077ffff"))
+    }
+    
+    func testImportVerifyProperties() {
+        guard let file = file,
+            let cert = Certificate.from(file: file) else {
+                XCTFail("Failed to laod the test file in CertificateTests")
+                return
+        }
+        let verifyData = cert.verifyData
+        
+        XCTAssertEqual(verifyData.type, "ECDSA(secp256k1)")
+        XCTAssertEqual(verifyData.signedAttribute, "uid")
+        XCTAssertEqual(verifyData.signer, URL(string: "https://www.theissuer.edu/keys/signing-public-key.asc"))
+    }
+}
