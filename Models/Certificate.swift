239911c0b6c540dc659eba28db609a438e6ae080
Chris Downie <cdownie@gmail.com>
2016-08-16 15:01:55 -0600

    Failing tests for signed and unsigned v1.2 certs.

--- Models/Certificate.swift
+++ Models/Certificate.swift
@@ -16,6 +16,10 @@ enum RevokeError : Error {
 // This enum is used to encapsuate the parse function. This is so CertificateParser is never instantiated.
 enum CertificateParser {
     static func parse(data: Data) -> Certificate? {
+        // Try to instantiate the latest version of the certificate format. Keep going through older versions until we've tried all of them or until we've found something that parses correctly
+        if let v1_2 = CertificateV1_2(data: data) {
+            return v1_2
+        }
         return CertificateV1_1(data: data)
     }
 }
@@ -207,6 +211,145 @@ struct CertificateV1_1 : Certificate {
             let recipient = MethodsForV1_1.parse(recipientJSON: json["recipient"]),
             let assertion = MethodsForV1_1.parse(assertionJSON: json["assertion"]),
             let verifyData = MethodsForV1_1.parse(verifyJSON: json["verify"]) else {
+                return nil
+        }
+        self.issuer = issuer
+        self.recipient = recipient
+        self.assertion = assertion
+        self.verifyData = verifyData
+    }
+}
+
+// MARK: - Certificate Version 1.2
+private enum MethodsForV1_2 {
+    static func parse(issuerJSON: AnyObject?) -> Issuer? {
+        guard let issuerData = issuerJSON as? [String : String],
+            let issuerURLString = issuerData["url"],
+            let issuerURL = URL(string: issuerURLString),
+            let logoURI = issuerData["image:logo"],
+            let issuerEmail = issuerData["email"],
+            let issuerName = issuerData["name"],
+            let issuerId = issuerData["id"],
+            let issuerIdURL = URL(string: issuerId) else {
+                return nil
+        }
+        let logo = imageData(from: logoURI)
+        
+        return Issuer(name: issuerName,
+                      email: issuerEmail,
+                      image: logo,
+                      id: issuerIdURL,
+                      url: issuerURL,
+                      publicKey: "",
+                      publicKeyAddress: URL(string: "https://google.com")!,
+                      requestUrl: URL(string: "https://google.com")!)
+    }
+    
+    static func parse(recipientJSON: AnyObject?) -> Recipient? {
+        guard let recipientData = recipientJSON as? [String : AnyObject],
+            let identityType = recipientData["type"] as? String,
+            let familyName = recipientData["familyName"] as? String,
+            let givenName = recipientData["givenName"] as? String,
+            let isHashed = recipientData["hashed"] as? Bool,
+            let publicKey = recipientData["pubkey"] as? String,
+            let identity = recipientData["identity"] as? String else {
+                return nil
+        }
+        
+        return Recipient(givenName: givenName,
+                         familyName: familyName,
+                         identity: identity,
+                         identityType: identityType,
+                         isHashed: isHashed,
+                         publicKey: publicKey)
+    }
+    
+    static func parse(assertionJSON: AnyObject?) -> Assertion? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+        
+        guard let assertionData = assertionJSON as? [String : String],
+            let issuedOnString = assertionData["issuedOn"],
+            let issuedOnDate = dateFormatter.date(from: issuedOnString),
+            let signatureImageURI = assertionData["image:signature"],
+            let assertionId = assertionData["id"],
+            let assertionIdUrl = URL(string: assertionId),
+            let assertionUid = assertionData["uid"],
+            let evidence = assertionData["evidence"] else {
+                return nil
+        }
+        
+        let signatureImage = imageData(from: signatureImageURI)
+        return Assertion(issuedOn: issuedOnDate,
+                         signatureImage: signatureImage,
+                         evidence: evidence,
+                         uid: assertionUid,
+                         id: assertionIdUrl)
+    }
+    
+    static func parse(verifyJSON: AnyObject?) -> Verify? {
+        guard let verifyData = verifyJSON as? [String : String],
+            let signer = verifyData["signer"],
+            let signedAttribute = verifyData["attribute-signed"],
+            let type = verifyData["type"],
+            let signerUrl = URL(string: signer) else {
+                return nil
+        }
+        
+        return Verify(signer: signerUrl, signedAttribute: signedAttribute, type: type)
+    }
+}
+
+// TODO: Right now this is identical to v1.1, except the version string and the method scoped struct
+struct CertificateV1_2 : Certificate {
+    let version = "1.2"
+    let title : String
+    let subtitle : String?
+    let description: String
+    let image : Data
+    let language : String
+    let id : URL
+    
+    let issuer : Issuer
+    let recipient : Recipient
+    let assertion : Assertion
+    let verifyData : Verify
+    
+    init?(data: Data) {
+        // Deserialize JSON
+        var json: [String: AnyObject]
+        do {
+            try json = JSONSerialization.jsonObject(with: data, options: []) as! [String: AnyObject]
+        } catch {
+            return nil
+        }
+        
+        // Get any key properties on the Certificate object
+        guard let certificateData = json["certificate"] as? [String: AnyObject],
+            let title = certificateData["title"] as? String,
+            let subtitleMap = certificateData["subtitle"] as? [String : String],
+            let certificateImageURI = certificateData["image:certificate"] as? String,
+            let certificateIdString = certificateData["id"] as? String,
+            let certificateIdUrl = URL(string: certificateIdString),
+            let description = certificateData["description"] as? String else {
+                return nil
+        }
+        let certificateImage = imageData(from: certificateImageURI)
+        let subtitle = subtitleMap["display"] == "FALSE" ? nil : subtitleMap["content"]
+        
+        self.title = title
+        self.subtitle = subtitle
+        self.description = description
+        self.image = certificateImage
+        language = ""
+        id = certificateIdUrl
+        
+        
+        // Use helper methods to parse Issuer, Recipient, Assert, and Verify objects.
+        guard let issuer = MethodsForV1_2.parse(issuerJSON: certificateData["issuer"]),
+            let recipient = MethodsForV1_2.parse(recipientJSON: json["recipient"]),
+            let assertion = MethodsForV1_2.parse(assertionJSON: json["assertion"]),
+            let verifyData = MethodsForV1_2.parse(verifyJSON: json["verify"]) else {
                 return nil
         }
         self.issuer = issuer
--- cert-walletTests/CertificateV1_2Tests.swift
+++ cert-walletTests/CertificateV1_2Tests.swift
@@ -8,8 +8,9 @@
 
 import XCTest
 
-class CertificateV1_1Tests: XCTestCase {
-    let certificateFilename = "sample_unsigned_cert-1.1.0"
+class UnsignedCertificateV1_2Tests: XCTestCase {
+    let certificateFilename = "sample_unsigned_cert-1.2.0"
+    
     var file : Data? = nil
     
     override func setUp() {
@@ -28,6 +29,93 @@ class CertificateV1_1Tests: XCTestCase {
         super.tearDown()
         
         file = nil
+    }
+    
+    func testImportProperties() {
+        XCTAssertNotNil(file)
+        guard let file = file,
+            let cert = CertificateParser.parse(data: file) else {
+                XCTFail("Failed to laod the test file in CertificateTests")
+                return
+        }
+        XCTAssertEqual(cert.title, "Certificate title")
+        XCTAssertEqual(cert.subtitle, nil)
+        XCTAssertEqual(cert.description, "Certificate description")
+        XCTAssertEqual(cert.id, URL(string: "https://www.theissuer.edu/criteria/2016/05/certificate-type.json"))
+    }
+    
+    func testImportIssuerProperties() {
+        guard let file = file,
+            let cert = CertificateParser.parse(data: file) else {
+                XCTFail("Failed to laod the test file in CertificateTests")
+                return
+        }
+        let issuer = cert.issuer
+        
+        XCTAssertEqual(issuer.name, "Issuing Institution")
+        XCTAssertEqual(issuer.email, "issuer@theissuer.edu")
+        XCTAssertEqual(issuer.id, URL(string: "https://www.theissuer.edu/issuer/the-issuer.json"))
+        XCTAssertEqual(issuer.url, URL(string: "http://www.theissuer.edu"))
+    }
+    
+    func testImportRecipientProperties() {
+        guard let file = file,
+            let cert = CertificateParser.parse(data: file) else {
+                XCTFail("Failed to laod the test file in CertificateTests")
+                return
+        }
+        let recipient = cert.recipient
+        XCTAssertEqual(recipient.givenName, "RecipientFirstName")
+        XCTAssertEqual(recipient.familyName, "RecipientLastName")
+        XCTAssertEqual(recipient.identity, "recipient@domain.com")
+        XCTAssertEqual(recipient.identityType, "email")
+        XCTAssertEqual(recipient.isHashed, false)
+        XCTAssertEqual(recipient.publicKey, "n1EduLzKsTL1pM8Roz9vEV16AQnBdg9JCx")
+    }
+    
+    func testImportAssertionProperties() {
+        guard let file = file,
+            let cert = CertificateParser.parse(data: file) else {
+                XCTFail("Failed to laod the test file in CertificateTests")
+                return
+        }
+        let assertion = cert.assertion
+        
+        XCTAssertEqual(assertion.issuedOn, Date(timeIntervalSinceReferenceDate: 485978400))
+        XCTAssertEqual(assertion.evidence, "")
+        XCTAssertEqual(assertion.uid, "68656c6c6f636f6d7077ffff")
+        XCTAssertEqual(assertion.id, URL(string: "http://www.theissuer.edu/68656c6c6f636f6d7077ffff"))
+    }
+    
+    func testImportVerifyProperties() {
+        guard let file = file,
+            let cert = CertificateParser.parse(data: file) else {
+                XCTFail("Failed to laod the test file in CertificateTests")
+                return
+        }
+        let verifyData = cert.verifyData
+        
+        XCTAssertEqual(verifyData.type, "ECDSA(secp256k1)")
+        XCTAssertEqual(verifyData.signedAttribute, "uid")
+        XCTAssertEqual(verifyData.signer, URL(string: "https://www.theissuer.edu/keys/signing-public-key.asc"))
+    }
+}
+
+class SignedCertificateV1_2Tests: XCTestCase {
+    let certificateFilename = "sample_signed_cert-1.2.0"
+    
+    var file : Data? = nil
+    
+    override func setUp() {
+        super.setUp()
+        
+        let testBundle = Bundle(for: type(of: self))
+        guard let fileUrl = testBundle.url(forResource: certificateFilename, withExtension: "json") else {
+            file = nil
+            return
+        }
+        
+        file = try? Data(contentsOf: fileUrl)
     }
     
     func testImportProperties() {
