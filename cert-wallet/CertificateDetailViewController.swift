0554c8514763ade63db8a9d0d415594c6a5702bb
Chris Downie <cdownie@gmail.com>
2016-08-17 14:18:57 -0600

    Adds dummy view controller for viewing a certificate in more detail.

--- cert-wallet/CertificateDetailViewController.swift
+++ cert-wallet/CertificateDetailViewController.swift
@@ -1,43 +1,43 @@
 //
-//  SecondViewController.swift
+//  CertificateDetailViewController.swift
 //  cert-wallet
 //
-//  Created by Chris Downie on 8/8/16.
-//  Copyright © 2016 Digital Certificates Project.
+//  Created by Chris Downie on 8/17/16.
+//  Copyright © 2016 Digital Certificates Project. All rights reserved.
 //
 
 import UIKit
 
-class CertificatesViewController: UITableViewController {
-    let cellReuseIdentifier = "CertificateTableViewCell"
-    let detailSegueIdentifier = "CertificateDetail"
-
+class CertificateDetailViewController: UITableViewController {
+    let cellReuseIdentifier = "CertificateDetailTableViewCell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         title = "Certificates"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 4
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 2
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier)!
-
+        
         cell.textLabel?.text = "Title \(indexPath.row)"
         cell.detailTextLabel?.text = "Subtitle \(indexPath.row)"
-
+        
         return cell
     }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        navigationController?.performSegue(withIdentifier: detailSegueIdentifier, sender: self)
-    }
+    
 }
 
