0e4422ed2b9b5927d835bf5625857818bc2d5bed
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-08-18 15:58:21 -0700

    add validation flow with placeholders

--- cert-walletTests/CertificateValidatorTests.swift
+++ cert-walletTests/CertificateValidatorTests.swift
@@ -12,30 +12,32 @@ import XCTest
 enum CertificateValidator {
     //
     static func validate(transactionId: String, certificate: Certificate) -> Bool {
-        
-        
+    
         let transactionUrl: URL = URL(string: String("https://blockchain.info/rawtx/" + transactionId + "?cors=true"))!
         let issuerUrl : URL = certificate.issuer.id
         
         let transactionData : Data = doRequest(theUrl : transactionUrl)
         let issuerKeyData : Data = doRequest(theUrl: issuerUrl)
 
+        // note that we need transactionOutputs for later; we need to determine if the revocation output is unspent...
+        let transactionOutputs : Array<???> = parseTransactionOutputs(data: transactionData)
+        // ...so parseOpReturn could just look for the transactionOutput with 0 output instead of re-parsing the json output
         let remoteHash : String = parseOpReturn(data: transactionData)
-        let issuerKeys : JSON = parseIssuerKeys(data: issuerKeyData)
-
-        let issuing_address = issuerKeys["issuer_key"][0]["key"]
-        let revocation_address = issuerKeys["revocation_key"][0]["key"]
+        let issuerKeys : IssuerKeys = parseIssuerKeys(data: issuerKeyData)
+        
+        print(remoteHash)
+        print(issuerKeys.issuerKey)
         
         // compare local and remote hashes
-        let local_hash = computeHash(certificate)
-        let compare_hash_result = compare_hashes(local_hash, remoteHash)
+        let localHash : String = computeHash(certificate)
+        let compareHashResult : Bool = compareHashes(localHash, remoteHash)
 
 
         // verify author signature
-        let verify_authors = checkAuthor(issuing_address, signed_local_json)
+        let authorSigned : Bool = checkAuthor(issuerKey: issuerKeys.issuerKey, certificate: certificate)
         
-        not_revoked = checkRevocation(remote_json, revocation_address)
-        return false
+        let notRevoked = checkRevocation(revocationAddress: issuerKeys.revokeKey, transactionOutputs : )
+        return compareHashResult && authorSigned && notRevoked
     }
     
     
@@ -67,7 +69,17 @@ enum CertificateValidator {
         semaphore.wait()
         return returnData
     }
-    
+ 
+    static func parseTransactionOutputs(data:Data) -> String {
+        do {
+            let json  = try JSONSerialization.jsonObject(with: data) as! [String: AnyObject]
+            if let outputs = json["out"] as? [[String: AnyObject]] {
+                return outputs
+            }
+        } catch {
+            print("error serializing JSON: \(error)")
+        }
+    }
     
     static func parseOpReturn(data:Data) -> String {
         do {
@@ -76,6 +88,7 @@ enum CertificateValidator {
                 for output in outputs {
                     if output["value"] as? Int == 0 {
                         print(output["script"])
+                        // convert this value to a hex string (python's hexlify) and return
                     }
                 }
             }
@@ -84,21 +97,23 @@ enum CertificateValidator {
         }
     }
     
-    static func parseIssuerKeys(data:Data) -> JSON {
+    
+    static func parseIssuerKeys(data:Data) -> IssuerKeys {
         do {
             let json  = try JSONSerialization.jsonObject(with: data) as! [String: AnyObject]
-            return json
+            let issuing_address : String = json["issuer_key"][0]["key"] as? [[String: AnyObject]]
+            let revocation_address : String = json["revocation_key"][0]["key"] as? [[String: AnyObject]]
+            let issuerKeys : IssuerKeys = IssuerKeys(issuerKey: issuing_address, revokeKey: revocation_address)
+            return issuerKeys
         } catch {
             print("error serializing JSON: \(error)")
         }
     }
     
-     
-     
-     static func check_revocation(tx_json, revoke_address) {
-        tx_outs = tx_json["out"]
-        for o in tx_outs {
-            if o.get("addr") == revoke_address and o.get("spent") is False {
+    
+    static func checkRevocation(revocationAddress : String, transactionOutputs : Array) {
+        for o in transactionOutputs {
+            if o.get("addr") == revocationAddress and o.get("spent") is False {
                 return True
             }
         }
@@ -107,6 +122,7 @@ enum CertificateValidator {
      
      
      static func computeHash(certificate) {
+        // this takes a SHA256 hash of the raw certificate (file)
         let doc_bytes = certificate
         if not isinstance(doc, (bytes, bytearray)) {
             doc_bytes = doc.encode("utf-8")
@@ -115,16 +131,13 @@ enum CertificateValidator {
         }
      
      
-     static func fetch_hash_from_chain(tx_json) {
-        let hash_from_bc = hexlify(get_hash_from_bc_op(tx_json))
-        return hash_from_bc
-     }
-     
-     static func compare_hashes(hash1, hash2) {
+     static func compareHashes(hash1, hash2) {
+        // we pass the local and remote hash (remote is what we got from the transaction lookup) and see if they match
         return hash1 in hash2 or hash1 == hash2
      }
      
-     static func check_author(address, signed_json) -> Bool {
+     static func checkAuthor(issuerKey : String, certificate : Certificate) -> Bool {
+        // verify that the certificate was signed by the issuer's issuerKey
         let uid = signed_json["assertion"]["uid"]
         let message = BitcoinMessage(uid)
         if signed_json.get("signature", None) {
@@ -138,6 +151,10 @@ enum CertificateValidator {
     
 }
 
+struct IssuerKeys {
+    let issuerKey : String
+    let revokeKey : String
+}
 
 class CertificateValidatorTests: XCTestCase {
     let v1_1filename = "sample_signed_cert-1.1.0"
