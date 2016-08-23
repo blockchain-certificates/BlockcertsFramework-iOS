5c97e6ab51b69fc5a5ea8fd1b8a0b6684d5e1e35
Chris Downie <cdownie@gmail.com>
2016-08-23 17:12:07 -0600

    A solid implementation of the certificate view, rendering in the issuers view.

---
+++ RenderedCertificateView/RCView.swift
@@ -0,0 +1,28 @@
+//
+//  RCView.swift
+//  cert-wallet
+//
+//  Created by Chris Downie on 8/23/16.
+//  Copyright © 2016 Digital Certificates Project. All rights reserved.
+//
+
+import UIKit
+
+class RCView: UIView {
+
+    @IBOutlet var view: UIView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var issuerIcon: UIImageView!
+    @IBOutlet weak var leftSignature: UIImageView!
+    @IBOutlet weak var sealIcon: UIImageView!
+    @IBOutlet weak var rightSignature: UIImageView!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        Bundle.main.loadNibNamed("RCView", owner: self, options: nil)
+        addSubview(view)
+    }
+
+}
