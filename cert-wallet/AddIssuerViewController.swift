3108592e031b77857db13123de89e812923ac344
Chris Downie <cdownie@gmail.com>
2016-08-25 13:44:21 -0600

    Constraints & form set up for adding an issuer.

--- cert-wallet/AddIssuerViewController.swift
+++ cert-wallet/AddIssuerViewController.swift
@@ -12,8 +12,10 @@ class AddIssuerViewController: UIViewController {
 
     @IBOutlet weak var saveButton: UIBarButtonItem!
     
-    @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var publicKeyURLField: UITextField!
+    @IBOutlet weak var firstNamefield: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var emailAddressField: UITextField!
     
     @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
         dismiss(animated: true, completion: nil)
@@ -26,18 +28,58 @@ class AddIssuerViewController: UIViewController {
         validate()
     }
     
-    func validate() {
-        let nameCharacterCount = nameField.text?.characters.count ?? 0
-        let publicKeyCharacterCount = publicKeyURLField.text?.characters.count ?? 0
-        if nameCharacterCount > 0,
-            publicKeyCharacterCount > 0,
-            URL(string: publicKeyURLField.text!) != nil {
-            saveButton.isEnabled = true
-        } else {
-            saveButton.isEnabled = false
-        }
+    // MARK - Form validation functions
+    private func validate() {
+        saveButton.isEnabled = areAllFieldsValid()
     }
-
+    
+    private func areAllFieldsValid() -> Bool {
+        guard isIssuerURLValid() else {
+            return false
+        }
+        return true
+    }
+    
+    private func isIssuerURLValid() -> Bool {
+        guard let issuerURLString = publicKeyURLField.text else {
+            return false
+        }
+        guard URL(string: issuerURLString) != nil else {
+            return false
+        }
+        return true
+    }
+    
+    private func isFirstNameValid() -> Bool {
+        guard let nameString = firstNamefield.text else {
+            return false
+        }
+        guard nameString.characters.count > 0 else {
+            return false
+        }
+        return true
+    }
+    
+    private func isLastNameValid() -> Bool {
+        guard let nameString = lastNameField.text else {
+            return false
+        }
+        guard nameString.characters.count > 0 else {
+            return false
+        }
+        return true
+    }
+    
+    private func isEmailAddressValid() -> Bool {
+        guard let nameString = emailAddressField.text else {
+            return false
+        }
+        guard nameString.characters.count > 0 else {
+            return false
+        }
+        // TODO: Check that this is a valid email.
+        return true
+    }
     /*
     // MARK: - Navigation
 
