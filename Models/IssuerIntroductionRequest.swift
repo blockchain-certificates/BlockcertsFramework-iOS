f5324939eb44cf6af02c66f28a847add79ec8cb9
Chris Downie <cdownie@gmail.com>
2016-09-02 11:39:14 -0600

    Now making a real request, but this isn't supported by our mock server yet. Up next...

--- Models/IssuerIntroductionRequest.swift
+++ Models/IssuerIntroductionRequest.swift
@@ -12,18 +12,36 @@ class IssuerIntroductionRequest {
     var callback : ((Bool, String?) -> Void)?
     let url : URL
     
+    private var recipient : Recipient
     private var session : URLSessionProtocol
     private var currentTask : URLSessionDataTaskProtocol?
     
     init(introduce recipient: Recipient, to issuer: Issuer, session: URLSessionProtocol = URLSession.shared, callback: ((Bool, String?) -> Void)?) {
         self.callback = callback
         self.session = session
+        self.recipient = recipient
         // TODO: It may not make sense for these additional properties to be optional. Investigate so I can remove this force-unwrap.
         url = issuer.requestUrl!
     }
     
     func start() {
-        currentTask = session.dataTask(with: url) { [weak self] (data, response, error) in
+        // Create JSON body
+        let dataMap = [
+            "bitcoinAddress": recipient.publicKey,
+            "email": recipient.identity,
+            "firstName": recipient.givenName,
+            "lastName": recipient.familyName
+        ]
+        guard let data = try? JSONSerialization.data(withJSONObject: dataMap, options: []) else {
+            reportFailure("Failed to create the body for the request.")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = data
+        
+        currentTask = URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
             guard let response = response as? HTTPURLResponse,
                 response.statusCode == 200 else {
                     self?.reportFailure("Server responded with non-200 status.")
@@ -32,6 +50,16 @@ class IssuerIntroductionRequest {
             
             self?.reportSuccess()
         }
+        
+//        currentTask = session.dataTask(with: url) { [weak self] (data, response, error) in
+//            guard let response = response as? HTTPURLResponse,
+//                response.statusCode == 200 else {
+//                    self?.reportFailure("Server responded with non-200 status.")
+//                    return
+//            }
+//            
+//            self?.reportSuccess()
+//        }
         currentTask?.resume()
     }
     
