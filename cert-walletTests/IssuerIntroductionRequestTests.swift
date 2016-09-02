a8627cd1d893e66340bc06a0cced1b3dd709fb92
Chris Downie <cdownie@gmail.com>
2016-09-02 13:10:46 -0600

    We now check the post data in the IssuerIntroductionRequest tests.

--- cert-walletTests/IssuerIntroductionRequestTests.swift
+++ cert-walletTests/IssuerIntroductionRequestTests.swift
@@ -13,6 +13,11 @@ class IssuerIntroductionRequestTests: XCTestCase {
         let itShouldCallTheCallback = expectation(description: "The request's callback handler will be called.")
         let itShouldCallTheServer = expectation(description: "Mocking framework should call our fake server function.")
         
+        let expectedAddress = "FakeRecipientPublicKey"
+        let expectedEmail = "johnny@blockcerts.org"
+        let expectedFirstName = "Johnny"
+        let expectedLastName = "Strong"
+        
         let issuer = Issuer(name: "BlockCerts Issuer",
                             email: "issuer@blockcerts.org",
                             image: "data:image/png;base64,".data(using: .utf8)!,
@@ -21,23 +26,32 @@ class IssuerIntroductionRequestTests: XCTestCase {
                             publicKey: "FakeIssuerPublicKey",
                             publicKeyAddress: URL(string: "https://blockcerts.org/pubkey")!,
                             requestUrl: URL(string: "https://blockcerts.org/introduce/")!)
-        let recipient = Recipient(givenName: "Johnny",
-                                  familyName: "Strong",
-                                  identity: "johnny@blockcerts.org",
+        let recipient = Recipient(givenName: expectedFirstName,
+                                  familyName: expectedLastName,
+                                  identity: expectedEmail,
                                   identityType: "email",
                                   isHashed: false,
-                                  publicKey: "FakeRecipientPublicKey")
+                                  publicKey: expectedAddress)
         
         // Mock out the network
         let session = MockURLSession()
         let url = issuer.requestUrl!
         session.respond(to: url) { request in
+            let body = request.httpBody
+            XCTAssertNotNil(body, "Request to the issuer should have a body.")
+            
+            let json = try? JSONSerialization.jsonObject(with: body!, options: [])
+            XCTAssertNotNil(json, "Body of the request should be parsable as json")
+            
+            let map = json as? [String: String]
+            XCTAssertNotNil(map, "Currently, the json is always String:String type")
+            
+            XCTAssertEqual(map!["bitcoinAddress"], expectedAddress)
+            XCTAssertEqual(map!["email"], expectedEmail)
+            XCTAssertEqual(map!["firstName"], expectedFirstName)
+            XCTAssertEqual(map!["lastName"], expectedLastName)
+
             itShouldCallTheServer.fulfill()
-            
-            XCTAssertEqual(request.url, url)
-            
-            // TODO: Check the post body of the request.
-            
             return (
                 data: "Success".data(using: .utf8),
                 response: HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil),
