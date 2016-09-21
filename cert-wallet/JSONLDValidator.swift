4c174b038ef000e86f1614f53f6208c798bdbfe1
Chris Downie <cdownie@gmail.com>
2016-09-21 11:39:06 -0600

    WIP trying to use WKWebView with the jsonld validator.

--- cert-wallet/JSONLDValidator.swift
+++ cert-wallet/JSONLDValidator.swift
@@ -7,18 +7,22 @@
 //
 
 import Foundation
-import JavaScriptCore
+//import JavaScriptCore
+import WebKit
 
 class JSONLDValidator {
     static let shared = JSONLDValidator()
 
-    let context : JSContext
+    // JSContext is not sufficient. We need web access 
+    let webView : WKWebView
+//    let context : JSContext
     
     init() {
-        guard let context = JSContext() else {
-            fatalError("Couldn't create a Javascript execution context in \(#file)")
-        }
-        self.context = context
+        webView = WKWebView(frame: .zero)
+        
+        // To conserve power, the WKWebView won't execute unless it's in the currently displayed view heirarchy.
+//        UIApplication.shared.keyWindow?.addSubview(webView)
+        
         
         guard let path = Bundle.main.path(forResource: "jsonld", ofType: "js") else {
             fatalError("Couldn't find `jsonld.js` file in \(#file)")
@@ -27,12 +31,23 @@ class JSONLDValidator {
             fatalError("Couldn't read contents of `jsonld.js` file in \(#file)")
         }
 
-        _ = context.evaluateScript(source)
+        webView.evaluateJavaScript(source) { (result, error) in
+            if let error = error {
+                print("Error: \(error)")
+            }
+        }
         
+        let demoTest = "jsonld.compact({  \"http://schema.org/name\": \"Manu Sporny\",  \"http://schema.org/url\": {\"@id\": \"http://manu.sporny.org/\"},  \"http://schema.org/image\": {\"@id\": \"http://manu.sporny.org/images/manu.png\"}}, {  \"name\": \"http://schema.org/name\",  \"homepage\": {\"@id\": \"http://schema.org/url\", \"@type\": \"@id\"},  \"image\": {\"@id\": \"http://schema.org/image\", \"@type\": \"@id\"}}, function(err, compacted) { console.log(err, compacted) })"
+        
+        webView.evaluateJavaScript(demoTest) { (result, error) in
+            if let error = error {
+                print("Error: \(error)")
+            }
+        }
     }
     
     func isValid(json: [String: Any]) -> Bool {
         return true
-        // TODO: Call the appropriate method in that context. 
+        // TODO: Call the appropriate method in that context.
     }
 }
