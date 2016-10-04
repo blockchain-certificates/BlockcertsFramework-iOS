85da426a0ed4c8a016fd6ccbba7e5548584c434a
Chris Downie <cdownie@gmail.com>
2016-10-04 11:39:19 -0600

    Creates BlockchainCertificate framework to place all our models in.

---
+++ BlockchainCertificates/BlockchainCertificates.h
@@ -0,0 +1,19 @@
+//
+//  BlockchainCertificates.h
+//  BlockchainCertificates
+//
+//  Created by Chris Downie on 10/4/16.
+//  Copyright © 2016 Digital Certificates Project. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BlockchainCertificates.
+FOUNDATION_EXPORT double BlockchainCertificatesVersionNumber;
+
+//! Project version string for BlockchainCertificates.
+FOUNDATION_EXPORT const unsigned char BlockchainCertificatesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BlockchainCertificates/PublicHeader.h>
+
+
