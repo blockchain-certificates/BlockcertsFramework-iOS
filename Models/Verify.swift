de5f5a9a2d13c0b924e1df73ecaf9385982ed081
Chris Downie <cdownie@gmail.com>
2016-10-04 15:22:22 -0600

    Verify model now in the BlockchainCertificates framework.

--- Models/Verify.swift
+++ Models/Verify.swift
@@ -9,13 +9,19 @@
 import Foundation
 
 /// Representing any data needed to verify a certificate.
-struct Verify {
+public struct Verify {
     /// URI where issuer's public key is presented.
-    let signer : URL
+    public let signer : URL
     
     /// Name of the attribute in the json that is signed by the issuer's private key. Default is `"uid"`, referring to the uid attribute.
-    let signedAttribute : String
+    public let signedAttribute : String
     
     /// Name of the signing method. Default is `"ECDSA(secp256k1)"`, referring to the Bitcoin method of signing messages with the issuer's private key.
-    let type : String
+    public let type : String
+    
+    public init(signer: URL, signedAttribute: String, type: String) {
+        self.signer = signer
+        self.signedAttribute = signedAttribute
+        self.type = type
+    }
 }
