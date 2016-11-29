934cd0116cb5d850ae082a69deb60390f6c2fcd1
Chris Downie <cdownie@gmail.com>
2016-11-29 14:55:55 -0700

    Well documented new Assertion initializer

--- Frameworks/BlockchainCertificates/Assertion.swift
+++ Frameworks/BlockchainCertificates/Assertion.swift
@@ -18,7 +18,10 @@ public struct Assertion {
     public let issuedOn : Date
     
     /// A base-64 encoded png image of the issuer's signature.
-    public let signatureImage : Data
+    public var signatureImage : Data? {
+        return signatureImages.first?.image
+    }
+    /// An array of base-64 encoded png images, paired with a title for each signature.
     public let signatureImages : [ SignatureImage ]
     
     /// Text, uri, etc. that shows evidence of the recipient's learning that the certificate represents. Can be left as an empty string if not used.
@@ -33,20 +36,24 @@ public struct Assertion {
     /// Public memberwise initializer. See above documentation for an explanation of each argument
     ///
     /// - returns: an initialized Assertion object.
-    /// This is deprecated. Use the one below with signatureImages
     public init(issuedOn: Date, signatureImage: Data, evidence: String, uid: String, id: URL) {
         self.issuedOn = issuedOn
-        self.signatureImage = signatureImage
         self.evidence = evidence
         self.uid = uid
         self.id = id
         signatureImages = [SignatureImage(image: signatureImage, title: nil)]
     }
     
-    // This is the
+    /// Public memberwise initializer.
+    ///
+    /// - Parameters:
+    ///   - issuedOn: Date the the certificate JSON was created.
+    ///   - signatureImages: An array of base-64 encoded png images, paired with a title for each signature.
+    ///   - evidence: Text, uri, etc. that shows evidence of the recipient's learning that the certificate represents. Can be left as an empty string if not used.
+    ///   - uid: Unique identifier. By default it is created using the string of a BSON ObjectId(), yielding an identifier 24 characters long.
+    ///   - id: URI that links to the certificate on the viewer. Default is https://[domain]/[uid]
     public init(issuedOn: Date, signatureImages: [SignatureImage], evidence: String, uid: String, id: URL) {
         self.issuedOn = issuedOn
-        self.signatureImage = signatureImages.first!.image
         self.evidence = evidence
         self.uid = uid
         self.id = id
