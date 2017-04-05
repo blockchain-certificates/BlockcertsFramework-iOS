4432640f77799b1ef66e5f0932962d3996110082
Chris Downie <cdownie@gmail.com>
2017-04-05 15:14:57 -0600

    Passes first test.

--- Frameworks/BlockchainCertificates/Metadata.swift
+++ Frameworks/BlockchainCertificates/Metadata.swift
@@ -12,12 +12,38 @@ import Foundation
 public struct Metadata {
     public static let visiblePathsKey = "displayOrder"
     
+    // TODO: This should just be a getter for the keys on groupedMetadata.
     let groups : [String]
-    
+    private let groupedMetadata : [String: [Metadatum]]
+
     
     init(json : [String: Any]) {
-        groups = [];
+        var groupedMetadata = [String : [Metadatum]]()
         
+        json.forEach { (key: String, value: Any) in
+            guard key != Metadata.visiblePathsKey else {
+                // If the visiblePathsKey is here, just skip it for now
+                return
+            }
+            guard let pairedValues = value as? [String: Any] else {
+                // This is an error condition, but let's hide the error for now.
+                return
+            }
+            
+            groupedMetadata[key] = pairedValues.map { (key: String, value: Any) -> Metadatum in
+                var type = MetadatumType.unknown
+                var stringValue : String = ""
+                switch (value) {
+                case let typedValue as String:
+                    type = .string
+                    stringValue = typedValue
+                default:
+                    stringValue = "\(value)"
+                    break;
+                }
+                return Metadatum(type: type, label: key, value: stringValue)
+            }
+        }
         // Go through all the keys
         // `displayOrder` is special -- save that for last
         // Everything else:
@@ -25,10 +51,14 @@ public struct Metadata {
         //   2. Create a Metadatum object for each child
         
         // Now go through `displayOrder`:
+        
+
+        groups = Array(groupedMetadata.keys)
+        self.groupedMetadata = groupedMetadata
     }
     
     func metadataFor(group: String) -> [Metadatum] {
-        return []
+        return groupedMetadata[group] ?? []
     }
     
     func metadatumFor(dotPath: String) -> Metadatum? {
@@ -41,6 +71,9 @@ public struct Metadata {
 }
 
 public enum MetadatumType {
+    // Error type
+    case unknown
+    
     // Basic types
     case number, string, date, boolean
     
