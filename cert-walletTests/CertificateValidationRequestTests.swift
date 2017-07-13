49e2cad229639115ddad664ba24d79164f3c8087
Chris Downie <cdownie@gmail.com>
2017-07-13 10:46:45 -0600

    Refactors the certificate validation requests that would've made my fixes earlier today easier to find.

--- cert-walletTests/CertificateValidationRequestTests.swift
+++ cert-walletTests/CertificateValidationRequestTests.swift
@@ -30,7 +30,6 @@ class CertificateValidationRequestTests: XCTestCase {
         let session = MockURLSession()
         
         let transactionURL = URL(string: "https://api.blockcypher.com/v1/btc/main/txs/\(v1_1transactionId)")!
-//        let transactionURL = URL(string:"https://blockchain.info/rawtx/\(v1_1transactionId)?cors=true")!
         let transactionResponse = HTTPURLResponse(url: transactionURL, statusCode: 200, httpVersion: nil, headerFields: nil)!
         let transactionData = "{\"block_hash\":\"000000000000000003700c5037f87c081e132a0516b297d8654c5274b1608275\",\"block_height\":413966,\"block_index\":71,\"hash\":\"d5df311055bf0fe656b9d6fa19aad15c915b47303e06677b812773c37050e35d\",\"addresses\":[\"18DmEbKzRBo6gQz9ET6hQeWUDcUbGAMqzC\",\"1HVE4FE9jqEe914WYhtZ9VzAJpjC4nuCig\",\"1JQ7ma9qcL2EZDMAnKneKeNXGU3WFoNtjg\"],\"total\":13025,\"fees\":10000,\"size\":302,\"preference\":\"low\",\"relayed_by\":\"76.112.113.11:8333\",\"confirmed\":\"2016-05-29T19:03:26Z\",\"received\":\"2016-05-29T18:58:25.315Z\",\"ver\":1,\"lock_time\":0,\"double_spend\":false,\"vin_sz\":1,\"vout_sz\":4,\"confirmations\":61698,\"confidence\":1,\"data_protocol\":\"unknown\",\"inputs\":[{\"prev_hash\":\"572e50582bbe533b1446c5aa5c18b2ffccf41fbb4ff973eb91dbb65a128858f9\",\"output_index\":2,\"script\":\"47304402207c7f06af8b8b8ba1c349d63252b3b4570370202168b3afbad3319695b609986a02201750f276c4d534904ba69342d7a97d371e28131c0b2c704fb5093aa0fe1e6255012103cc68e3493cfe9467d7ef6da336ed554f4425a4450f062048d18860ee68556ebe\",\"output_value\":23025,\"sequence\":4294967295,\"addresses\":[\"1HVE4FE9jqEe914WYhtZ9VzAJpjC4nuCig\"],\"script_type\":\"pay-to-pubkey-hash\"}],\"outputs\":[{\"value\":2750,\"script\":\"76a9144f32cc080424addd43421ad862ae327bb84d894888ac\",\"addresses\":[\"18DmEbKzRBo6gQz9ET6hQeWUDcUbGAMqzC\"],\"script_type\":\"pay-to-pubkey-hash\"},{\"value\":2750,\"script\":\"76a914bed95b01064d287267bb49b44de2ce577ca5468f88ac\",\"addresses\":[\"1JQ7ma9qcL2EZDMAnKneKeNXGU3WFoNtjg\"],\"script_type\":\"pay-to-pubkey-hash\"},{\"value\":7525,\"script\":\"76a914b4d88edb2677121b603b8ee1326247aaad45cbea88ac\",\"spent_by\":\"25a54e5a6beb90ae30ea7797108939ee2aea50b671652ec0d8ed14f0612b0b51\",\"addresses\":[\"1HVE4FE9jqEe914WYhtZ9VzAJpjC4nuCig\"],\"script_type\":\"pay-to-pubkey-hash\"},{\"value\":0,\"script\":\"6a20b200240216e10d988bc748e675abb1b5cc1b29384e9f1c68cad9f5a03a6ed531\",\"addresses\":null,\"script_type\":\"null-data\",\"data_hex\":\"b200240216e10d988bc748e675abb1b5cc1b29384e9f1c68cad9f5a03a6ed531\"}]}".data(using: .utf8)
         session.respond(to: transactionURL,
@@ -62,12 +61,10 @@ class CertificateValidationRequestTests: XCTestCase {
         
         let testBundle = Bundle(for: type(of: self))
         guard let fileUrl = testBundle.url(forResource: v1_1ValidFilename, withExtension: "json") ,
-            let file = try? Data(contentsOf: fileUrl) else {
-                return
-        }
-        
-        guard let txUrl = testBundle.url(forResource: v1_1txFilename, withExtension: "json") ,
+            let file = try? Data(contentsOf: fileUrl),
+            let txUrl = testBundle.url(forResource: v1_1txFilename, withExtension: "json") ,
             let txFile = try? Data(contentsOf: txUrl) else {
+                XCTFail("Couldn't find test resources. This test is misconfigured.")
                 return
         }
         
@@ -107,21 +104,14 @@ class CertificateValidationRequestTests: XCTestCase {
         
         let testBundle = Bundle(for: type(of: self))
         guard let fileUrl = testBundle.url(forResource: v1_2ValidFilename, withExtension: "json") ,
-            let file = try? Data(contentsOf: fileUrl) else {
-                return
-        }
-        guard let txUrl = testBundle.url(forResource: v1_2txFilename, withExtension: "json") ,
-            let txFile = try? Data(contentsOf: txUrl) else {
-                return
-        }
-        
-        guard let issuerUrl = testBundle.url(forResource: gotIssuerFilename, withExtension: "json") ,
-            let issuerFile = try? Data(contentsOf: issuerUrl) else {
-                return
-        }
-        
-        guard let normalizedUrl = testBundle.url(forResource: v1_2normalized, withExtension: "txt") ,
+            let file = try? Data(contentsOf: fileUrl),
+            let txUrl = testBundle.url(forResource: v1_2txFilename, withExtension: "json") ,
+            let txFile = try? Data(contentsOf: txUrl),
+            let issuerUrl = testBundle.url(forResource: gotIssuerFilename, withExtension: "json") ,
+            let issuerFile = try? Data(contentsOf: issuerUrl),
+            let normalizedUrl = testBundle.url(forResource: v1_2normalized, withExtension: "txt") ,
             let normalizedFile = try? Data(contentsOf: normalizedUrl) else {
+                XCTFail("Couldn't find test resources. This test is misconfigured.")
                 return
         }
         
@@ -170,27 +160,16 @@ class CertificateValidationRequestTests: XCTestCase {
         
         let testBundle = Bundle(for: type(of: self))
         guard let fileUrl = testBundle.url(forResource: certFilename, withExtension: "json") ,
-            let file = try? Data(contentsOf: fileUrl) else {
-                return
-        }
-        
-        guard let txUrl = testBundle.url(forResource: txFilename, withExtension: "json") ,
-            let txFile = try? Data(contentsOf: txUrl) else {
-                return
-        }
-        
-        guard let issuerUrl = testBundle.url(forResource: sampleIssuerFilename, withExtension: "json") ,
-            let issuerFile = try? Data(contentsOf: issuerUrl) else {
-                return
-        }
-        
-        guard let normalizedUrl = testBundle.url(forResource: normalizedFilename, withExtension: "txt") ,
-            let normalizedFile = try? Data(contentsOf: normalizedUrl) else {
-                return
-        }
-        
-        guard let revocationUrl = testBundle.url(forResource: revocationList, withExtension: "json") ,
+            let file = try? Data(contentsOf: fileUrl),
+            let txUrl = testBundle.url(forResource: txFilename, withExtension: "json") ,
+            let txFile = try? Data(contentsOf: txUrl),
+            let issuerUrl = testBundle.url(forResource: sampleIssuerFilename, withExtension: "json") ,
+            let issuerFile = try? Data(contentsOf: issuerUrl),
+            let normalizedUrl = testBundle.url(forResource: normalizedFilename, withExtension: "txt") ,
+            let normalizedFile = try? Data(contentsOf: normalizedUrl),
+            let revocationUrl = testBundle.url(forResource: revocationList, withExtension: "json") ,
             let revocationFile = try? Data(contentsOf: revocationUrl) else {
+                XCTFail("Couldn't find test resources. This test is misconfigured.")
                 return
         }
         
@@ -244,27 +223,16 @@ class CertificateValidationRequestTests: XCTestCase {
         
         let testBundle = Bundle(for: type(of: self))
         guard let fileUrl = testBundle.url(forResource: certFilename, withExtension: "json") ,
-            let file = try? Data(contentsOf: fileUrl) else {
-                return
-        }
-        
-        guard let txUrl = testBundle.url(forResource: txFilename, withExtension: "json") ,
-            let txFile = try? Data(contentsOf: txUrl) else {
-                return
-        }
-        
-        guard let issuerUrl = testBundle.url(forResource: sampleIssuerFilename, withExtension: "json") ,
-            let issuerFile = try? Data(contentsOf: issuerUrl) else {
-                return
-        }
-        
-        guard let normalizedUrl = testBundle.url(forResource: normalizedFilename, withExtension: "txt") ,
-            let normalizedFile = try? Data(contentsOf: normalizedUrl) else {
-                return
-        }
-        
-        guard let revocationUrl = testBundle.url(forResource: revocationList, withExtension: "json") ,
+            let file = try? Data(contentsOf: fileUrl),
+            let txUrl = testBundle.url(forResource: txFilename, withExtension: "json") ,
+            let txFile = try? Data(contentsOf: txUrl),
+            let issuerUrl = testBundle.url(forResource: sampleIssuerFilename, withExtension: "json") ,
+            let issuerFile = try? Data(contentsOf: issuerUrl),
+            let normalizedUrl = testBundle.url(forResource: normalizedFilename, withExtension: "txt") ,
+            let normalizedFile = try? Data(contentsOf: normalizedUrl),
+            let revocationUrl = testBundle.url(forResource: revocationList, withExtension: "json") ,
             let revocationFile = try? Data(contentsOf: revocationUrl) else {
+                XCTFail("Couldn't find test resources. This test is misconfigured.")
                 return
         }
         
@@ -318,27 +286,16 @@ class CertificateValidationRequestTests: XCTestCase {
         
         let testBundle = Bundle(for: type(of: self))
         guard let fileUrl = testBundle.url(forResource: certFilename, withExtension: "json") ,
-            let file = try? Data(contentsOf: fileUrl) else {
-                return
-        }
-        
-        guard let txUrl = testBundle.url(forResource: txFilename, withExtension: "json") ,
-            let txFile = try? Data(contentsOf: txUrl) else {
-                return
-        }
-        
-        guard let issuerUrl = testBundle.url(forResource: sampleIssuerFilename, withExtension: "json") ,
-            let issuerFile = try? Data(contentsOf: issuerUrl) else {
-                return
-        }
-        
-        guard let normalizedUrl = testBundle.url(forResource: normalizedFilename, withExtension: "txt") ,
-            let normalizedFile = try? Data(contentsOf: normalizedUrl) else {
-                return
-        }
-        
-        guard let revocationUrl = testBundle.url(forResource: revocationList, withExtension: "json") ,
+            let file = try? Data(contentsOf: fileUrl),
+            let txUrl = testBundle.url(forResource: txFilename, withExtension: "json") ,
+            let txFile = try? Data(contentsOf: txUrl),
+            let issuerUrl = testBundle.url(forResource: sampleIssuerFilename, withExtension: "json") ,
+            let issuerFile = try? Data(contentsOf: issuerUrl),
+            let normalizedUrl = testBundle.url(forResource: normalizedFilename, withExtension: "txt") ,
+            let normalizedFile = try? Data(contentsOf: normalizedUrl),
+            let revocationUrl = testBundle.url(forResource: revocationList, withExtension: "json") ,
             let revocationFile = try? Data(contentsOf: revocationUrl) else {
+                XCTFail("Couldn't find test resources. This test is misconfigured.")
                 return
         }
         
@@ -392,27 +349,16 @@ class CertificateValidationRequestTests: XCTestCase {
         
         let testBundle = Bundle(for: type(of: self))
         guard let fileUrl = testBundle.url(forResource: certFilename, withExtension: "json") ,
-            let file = try? Data(contentsOf: fileUrl) else {
-                return
-        }
-        
-        guard let txUrl = testBundle.url(forResource: txFilename, withExtension: "json") ,
-            let txFile = try? Data(contentsOf: txUrl) else {
-                return
-        }
-        
-        guard let issuerUrl = testBundle.url(forResource: sampleIssuerFilename, withExtension: "json") ,
-            let issuerFile = try? Data(contentsOf: issuerUrl) else {
-                return
-        }
-        
-        guard let normalizedUrl = testBundle.url(forResource: normalizedFilename, withExtension: "txt") ,
-            let normalizedFile = try? Data(contentsOf: normalizedUrl) else {
-                return
-        }
-        
-        guard let revocationUrl = testBundle.url(forResource: revocationList, withExtension: "json") ,
+            let file = try? Data(contentsOf: fileUrl),
+            let txUrl = testBundle.url(forResource: txFilename, withExtension: "json") ,
+            let txFile = try? Data(contentsOf: txUrl),
+            let issuerUrl = testBundle.url(forResource: sampleIssuerFilename, withExtension: "json") ,
+            let issuerFile = try? Data(contentsOf: issuerUrl),
+            let normalizedUrl = testBundle.url(forResource: normalizedFilename, withExtension: "txt") ,
+            let normalizedFile = try? Data(contentsOf: normalizedUrl),
+            let revocationUrl = testBundle.url(forResource: revocationList, withExtension: "json") ,
             let revocationFile = try? Data(contentsOf: revocationUrl) else {
+                XCTFail("Couldn't find test resources. This test is misconfigured.")
                 return
         }
         
@@ -466,27 +412,16 @@ class CertificateValidationRequestTests: XCTestCase {
         
         let testBundle = Bundle(for: type(of: self))
         guard let fileUrl = testBundle.url(forResource: v2ValidFilename, withExtension: "json") ,
-            let file = try? Data(contentsOf: fileUrl) else {
-                return
-        }
-        
-        guard let txUrl = testBundle.url(forResource: v2txFilename, withExtension: "json") ,
-            let txFile = try? Data(contentsOf: txUrl) else {
-                return
-        }
-        
-        guard let issuerUrl = testBundle.url(forResource: sampleIssuerFilename, withExtension: "json") ,
-            let issuerFile = try? Data(contentsOf: issuerUrl) else {
-                return
-        }
-        
-        guard let normalizedUrl = testBundle.url(forResource: v2normalized, withExtension: "txt") ,
-            let normalizedFile = try? Data(contentsOf: normalizedUrl) else {
-                return
-        }
-        
-        guard let revocationUrl = testBundle.url(forResource: v2revocationList, withExtension: "json") ,
+            let file = try? Data(contentsOf: fileUrl),
+            let txUrl = testBundle.url(forResource: v2txFilename, withExtension: "json") ,
+            let txFile = try? Data(contentsOf: txUrl),
+            let issuerUrl = testBundle.url(forResource: sampleIssuerFilename, withExtension: "json") ,
+            let issuerFile = try? Data(contentsOf: issuerUrl),
+            let normalizedUrl = testBundle.url(forResource: v2normalized, withExtension: "txt") ,
+            let normalizedFile = try? Data(contentsOf: normalizedUrl),
+            let revocationUrl = testBundle.url(forResource: v2revocationList, withExtension: "json") ,
             let revocationFile = try? Data(contentsOf: revocationUrl) else {
+                XCTFail("Couldn't find test resources. This test is misconfigured.")
                 return
         }
         
