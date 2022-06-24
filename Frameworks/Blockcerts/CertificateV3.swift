3b98250fd2297a81477265c2bd7e385ac3bbe3c1
Matthieu COLLÉ <druidev+github@gmail.com>
2022-06-24 10:21:28 -0400

    feat(V3): added ContentMediaType enum

---
+++ Frameworks/Blockcerts/CertificateV3.swift
@@ -0,0 +1,9 @@
+//
+//  CertificateV3.swift
+//  Blockcerts
+//
+//  Created by Matthieu Collé on 01/06/2022.
+//  Copyright © 2022 Digital Certificates Project. All rights reserved.
+//
+
+import Foundation
---
+++ Frameworks/Blockcerts/IssuerVersions/IssuerV3.swift
@@ -0,0 +1,9 @@
+//
+//  IssuerV3.swift
+//  Blockcerts
+//
+//  Created by Matthieu Collé on 23/06/2022.
+//  Copyright © 2022 Digital Certificates Project. All rights reserved.
+//
+
+import Foundation
